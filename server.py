4bc8bab9e792359ce970e93f988b9431d86362ff
Marcio Filho <marciof1555@gmail.com>
2022-06-29 02:03:56 -0300

    Juntando class Crawler no back

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from 
+from crawle import Crawler
 
 app = Flask(__name__)
 
@@ -11,10 +11,13 @@ def index():
 def passagem():
     origem = request.form.get('origem')
     destino = request.form.get('destino')
+    passageiros = request.form.get('passageiro')
     data_ida = request.form.get('data_ida')
     data_volta = request.form.get('data_volta')
     
-
+    crawler = Crawler()
+    res = crawler.send_dados(origem, destino, passageiros, data_ida, data_volta)
+    crawler.get_dados(res)
     
     return 'oi'
 
