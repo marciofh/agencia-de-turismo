b9d3bd20665c99284be64c1d22185f0ab143a1d3
Marcio Filho <marciof1555@gmail.com>
2022-07-05 21:38:28 -0300

    deletando arquivos

--- calc.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import re
-df = pd.read_excel('./EXTRATO_TOTAL.xlsx')
-
-df['preco'] = df['preco'].map(lambda x: x[3:10])
-df['preco'] = df['preco'].map(lambda x: re.sub('[^0-9,]', '', x))
-
-print(df)
